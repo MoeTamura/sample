cd71a1a4db7a4555d4fa1dae674fd581ffb6c8b1
MoeTamura <moe_buena_suerte@outlook.jp>
2023-09-07 15:54:44 +0900

    ブログ投稿作成処理実行時のバリデーション実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
+
 
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->get()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(Post $post)
@@ -18,15 +20,15 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-     public function store(Request $request, Post $post)
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Post $post, Request $request)
     {
        $input = $request['post'];
        $post->fill($input)->save();
        return redirect('/posts/' . $post->id);
-    }
-    
-    public function create()
-    {
-        return view('posts/create');
     }
 }
