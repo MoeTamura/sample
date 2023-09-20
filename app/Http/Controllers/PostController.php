510410d4e1929635e25a1758e90142104e8f2c86
MoeTamura <moe_buena_suerte@outlook.jp>
2023-09-20 22:52:11 +0900

    ブログ投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,23 +12,32 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
-    
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    
     public function create()
     {
         return view('posts.create');
     }
-    
-    public function store(Post $post, Request $request)
+     public function store(Post $post, Request $request)
     {
        $input = $request['post'];
        $post->fill($input)->save();
+       return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+       return view('posts/edit')->with(['post' => $post]);
+    }  
+    
+    public function update(PostRequest $request, Post $post)
+    {
+       $input_post = $request['post'];
+       $post->fill($input_post)->save();
+       
        return redirect('/posts/' . $post->id);
     }
 }
