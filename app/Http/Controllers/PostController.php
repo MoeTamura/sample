9f1fa5195fdd5f80a74b0538a62a30937c2cbf6f
MoeTamura <moe_buena_suerte@outlook.jp>
2023-10-15 12:46:23 +0900

    認証機能Breeze

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Models\Category;
 
 
 
@@ -12,17 +13,17 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(10)]);
     }
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts.create');
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
-     public function store(Post $post, Request $request)
+     public function store(Post $post, PostRequest $request)
     {
        $input = $request['post'];
        $post->fill($input)->save();
@@ -36,8 +37,7 @@ class PostController extends Controller
     public function update(PostRequest $request, Post $post)
     {
        $input_post = $request['post'];
-       $post->fill($input_post)->save();
-       
+       $post->fill($input)->save();
        return redirect('/posts/' . $post->id);
     }
     
